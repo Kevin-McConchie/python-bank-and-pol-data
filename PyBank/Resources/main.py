cba5b6b9712b36f4815791cfc3c026f7b646f160
Kevin-McConchie <102848403+Kevin-McConchie@users.noreply.github.com>
2022-05-22 17:09:52 +1000

    Save 5:09pm

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -15,45 +15,44 @@ with open (budget_data, newline="") as csv_file:
     csvheader= next(csv_file)
     #print(f"Header:{csv_header}")----------> unsure if neeeded
     
-    #Find number of months and profit in dataset
+    #create lists for calcuations
     month = []
     profit = []
-    
+    profit_change = []
+        
     for row in csvreader:
         month.append(row[0])
         profit.append(row[1])
     
+    for y in range(len(profit)-1):
+        profit_change.append(int(profit[y+1])-int(profit[y]))
+            
+
+#find total months
 total_months = len(month)
-total_profit = sum(int(x) for x in profit)
-print(str(total_profit))
+
 # The net total amount of "Profit/Losses" over the entire period
-# def average(profit):
-#     length = len(profit)
-#     total = 0.0
-#     for number in profit:
-#         total += number
-#     return total / length
-    
-# print (average(profit))
-
-# Test your function with the following:
-# print(average([1, 5, 9]))
-# print(average(range(11)))
-
+total_profit = sum(int(x) for x in profit)
 
 # The average of the changes in "Profit/Losses" over the entire period
+sum_profit_change = sum(profit_change)
+ave_change = round((sum_profit_change) / (total_months-1),2)
 
 
 # The greatest increase in profits (date and amount) over the entire period
-#def print_percentages(wrestler_data):
+max_increase = max(profit_change)
+month_increase = profit_change.index(max(profit_change))+1
 
 # The greatest decrease in losses (date and amount) over the entire period
-
-
+max_decrease = min(profit_change)
+month_decrease = profit_change.index(min(profit_change))+1
     
     
     #Print results
 print ("Financial Analysis")
 print ("----------------------------")
-print(f"Total Months : " + str(total_months))
-print(f"Total: $" +str(total_profit))
+print (f"Total Months : " + str(total_months))
+print (f"Total: $" +str(total_profit))
+print (f"Average Change: $"+str(ave_change))
+print (f"Greatest Increase in Profits "+ {month(month_increase)} +"($"+ str(max_increase)+")")
+print (f"Greatest Increase in Profits "+ {month(month_decrease)} +"($"+ str(max_decrease)+")")
